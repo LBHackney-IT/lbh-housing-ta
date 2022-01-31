64dd28ce56758656b9b33e652f35639c157bf316
David Thompson <davidt@hey.com>
2022-01-31 15:26:56 +0000

    Add stub assets page

---
+++ pages/assets.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Layout from "../components/Layout";
+
+const Assets: NextPage = () => {
+  return (
+    <Layout>
+      <h1 className="lbh-heading-h1">Assets</h1>
+    </Layout>
+  );
+};
+
+export default Assets;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
+import Link from 'next/link'
 import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <h1 className="lbh-heading-h1">Hackney temporary accomodation</h1>
+      <ul>
+        <li>
+          <Link href="/assets">
+            <a>Assets</a>
+          </Link>
+        </li>
+      </ul>
     </Layout>
   );
 };
