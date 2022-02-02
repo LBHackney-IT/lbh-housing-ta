407ae4dd2abbb54ee85a98d270dc342011ca5605
David Thompson <davidt@hey.com>
2022-02-02 09:07:27 +0000

    Extract pagination to component

--- pages/assets.tsx
+++ pages/assets.tsx
@@ -1,49 +1,12 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
+import Pagination from "../components/Pagination";
 
 const Assets: NextPage = () => {
   return (
     <Layout>
       <h1 className="lbh-heading-h1">Accommodation</h1>
-      <nav className="lbh-pagination">
-      <div className="lbh-pagination__summary">Showing 101—150 of 246 results</div>
-      <ul className="lbh-pagination">
-        <li className="lbh-pagination__item">
-          <a className="lbh-pagination__link" href="#" aria-label="Previous page">
-            <span aria-hidden="true" role="presentation">&laquo;</span>
-            Previous
-          </a>
-        </li>
-        <li className="lbh-pagination__item">
-          <a className="lbh-pagination__link" href="#" aria-label="Page 1">1</a>
-        </li>
-        <li className="lbh-pagination__item">
-          <a className="lbh-pagination__link" href="#" aria-label="Page 2">2</a>
-        </li>
-        <li className="lbh-pagination__item">
-          <a
-            className="lbh-pagination__link lbh-pagination__link--current"
-            href="#"
-            aria-current="true"
-            aria-label="Page 3, current page"
-          >
-            3
-          </a>
-        </li>
-        <li className="lbh-pagination__item">
-          <a className="lbh-pagination__link" href="#" aria-label="Page 4">4</a>
-        </li>
-        <li className="lbh-pagination__item">
-          <a className="lbh-pagination__link" href="#" aria-label="Page 5">5</a>
-        </li>
-        <li className="lbh-pagination__item">
-          <a className="lbh-pagination__link" href="#" aria-label="Next page">
-            Next
-            <span aria-hidden="true" role="presentation">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+      <Pagination />
     </Layout>
   );
 };
